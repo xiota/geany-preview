171bc27146ac13a242168be4850a987224d04cb3
xiota <github@mentalfossa.com>
2025-09-10 05:14:38 +0000

    feat(util): add trimWhitespaceView and escapeHtml to string_utils

--- source/util/string_utils.h
+++ source/util/string_utils.h
@@ -28,4 +28,42 @@ inline std::string trimWhitespace(std::string_view sv) {
   return std::string(sv.substr(b, e - b + 1));
 }
 
+inline std::string_view trimWhitespaceView(std::string_view sv) {
+  const char *ws = " \t\r\n";
+  auto b = sv.find_first_not_of(ws);
+  if (b == std::string_view::npos) {
+    return {};
+  }
+  auto e = sv.find_last_not_of(ws);
+  return sv.substr(b, e - b + 1);
+}
+
+inline std::string escapeHtml(std::string_view sv) {
+  std::string out;
+  out.reserve(sv.size() * 2);  // reasonable headroom for most cases
+  for (char c : sv) {
+    switch (c) {
+      case '&':
+        out += "&amp;";
+        break;
+      case '<':
+        out += "&lt;";
+        break;
+      case '>':
+        out += "&gt;";
+        break;
+      case '"':
+        out += "&quot;";
+        break;
+      case '\'':
+        out += "&#39;";
+        break;
+      default:
+        out.push_back(c);
+        break;
+    }
+  }
+  return out;
+}
+
 }  // namespace StringUtils
